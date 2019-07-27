d20f88b9deb7373a25cd9d77219bbbe55fab99e6
SihemBouhenniche <fs_bouhenniche@esi.dz>
2019-07-27 19:41:48 +0200

    fix ffi issue and url for google page

--- features/support/prism_pages/google_home_page.rb
+++ features/support/prism_pages/google_home_page.rb
@@ -1,7 +1,7 @@
 #e2e-tests/features/support/prism_pages/base/dashboard/admin_dashboard_pages.rb
 # Google home page object: load elements of google home page.
 class GoogleHomePage < SitePrism::Page
-  set_url '/'
+  set_url 'https://www.google.com'
   element :keyword_input, 'input.gLFyf'
   element :submit_button, 'input[name=btnK]'
   element :first_suggestion, '.suggestions-inner-container .sbl1'
