a5000ee474f5735419eb319b70ff3344082ee6b0
Manel <manel.adman@emploitic.com>
2019-07-29 14:57:37 +0100

    adding time and selection utilities files

--- features/support/prism_pages/utils_page.rb
+++ features/support/prism_pages/utils_page.rb
@@ -13,4 +13,22 @@ class UtilsPage < SitePrism::Page
   def find_all(selector)
     page.all(:css, selector)
   end
+
+  #@method execute_js(script)
+  #execute js script
+  def execute_js(script)
+    page.execute_script script
+  end
+
+  #@method evaluate_js(script)
+  #evaluate js script and return js return object
+  def evaluate_js(script)
+    page.evaluate_script script
+  end
+
+  #@method expect_to_have_selector(selector)
+  #check if selector in page
+  def expect_to_have_selector(selector)
+    page.has_selector? selector
+  end
 end
