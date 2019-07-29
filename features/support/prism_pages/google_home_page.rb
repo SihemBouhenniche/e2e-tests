0f4c49aea1cbc8de6c71f41d6f072fdd827765ce
Manel <manel.adman@emploitic.com>
2019-07-29 14:26:01 +0100

    adding capybara utils helpers

--- features/support/prism_pages/google_home_page.rb
+++ features/support/prism_pages/google_home_page.rb
@@ -1,7 +1,7 @@
-#e2e-tests/features/support/prism_pages/base/dashboard/admin_dashboard_pages.rb
+#e2e-tests/features/support/prism_pages/google_home_pages.rb
 # Google home page object: load elements of google home page.
 class GoogleHomePage < SitePrism::Page
-  set_url 'https://www.google.com'
+  set_url '/'
   element :keyword_input, 'input.gLFyf'
   element :submit_button, 'input[name=btnK]'
   element :first_suggestion, '.suggestions-inner-container .sbl1'
