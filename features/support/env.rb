ac6809d584c66d7aca97ed09da80c8fd3cd77093
Manel <manel.adman@emploitic.com>
2019-07-29 14:59:31 +0100

    adding faker support

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,11 +5,12 @@ require 'rspec'
 require 'rspec/expectations'
 require 'csv'
 require 'webdrivers'
+require 'faker'
 
 Capybara.default_max_wait_time = 10
 Capybara.app_host = ENV['host']
 Capybara.default_selector = :css
-Capybara.ignore_hidden_elements = false
+Capybara.ignore_hidden_elements = true
 Webdrivers.cache_time = 86_400
 
 Capybara.register_driver :firefox do |app|
